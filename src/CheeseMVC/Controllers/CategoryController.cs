3f06916a927aa64725bcab11e2b7591496b78f49
Marcy Bursac <msbursac@gmail.com>
2017-12-14 19:38:33 -0600

    Completed Studio Tasks for Part 1 thru View All Categories

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Data;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var allCategories = context.Categories.ToList();
+            return View(allCategories);
+        }
+
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+
+        {
+            context = dbContext;
+        }
+    }
+}
