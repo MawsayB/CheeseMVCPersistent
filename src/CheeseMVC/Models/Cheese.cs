a210d988d4ec18ba7e2561f6eed9925a435956c6
Marcy Bursac <msbursac@gmail.com>
2017-12-18 20:12:11 -0600

    Updated AddCheeseViewModel

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -3,6 +3,7 @@
     public class Cheese
     {
         public string Name { get; set; }
+        public int ID { get; set; }
         public string Description { get; set; }
         public CheeseCategory Category { get; set; }
         public int CategoryID { get; set; }
