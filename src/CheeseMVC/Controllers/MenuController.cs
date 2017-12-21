03258e02154ed6a9d1a12addd6093322637b426f
Marcy Bursac <msbursac@gmail.com>
2017-12-20 20:42:38 -0600

    Set up MenuController and private context field

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Data;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
